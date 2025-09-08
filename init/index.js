09ecda5323080e545bdd43acbd9a6d56535d075c
Anish Dwivedi <anish56808@gmail.com>
2025-09-08 11:34:44 +0530

    initial commit

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const mongo_url = "mongodb://127.0.0.1:27017/Managment_app";
+const mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
 
 const initDB = async () => {
     await Listing.deleteMany({});
